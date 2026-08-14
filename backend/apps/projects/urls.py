87777a7d29e42c9944a608f1632d2a223ce89360
Fabrizio Adrian Avila <fabri.avila3@gmail.com>
2026-08-14 12:50:40 -0300

    FEAT - Termina endpoint de postulacion de los testers

--- backend/apps/projects/urls.py
+++ backend/apps/projects/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
+from apps.postulations.views import ProjectPostulationCreateView
+
 from .views import ProjectCreateView
 
 
 urlpatterns = [
     path('', ProjectCreateView.as_view(), name='project-create'),
+      path(
+        '<int:id_project>/postulations/',
+        ProjectPostulationCreateView.as_view(),
+        name='project-postulation-create'
+    ),
 ]
