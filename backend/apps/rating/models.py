bc2a99e7b9e77aa13cdee89add6e083031727bc3
Carlos Huallpa <167722493+Huallpacl@users.noreply.github.com>
2026-08-17 22:26:23 -0300

    feat: agregado endpoint de update y validaciones del modelo(maximo 5 estrellas, comentarios)

--- backend/apps/rating/models.py
+++ backend/apps/rating/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
@@ -7,11 +8,11 @@ class Rating(models.Model):
 
     id_postulation=models.ForeignKey('postulations.Postulation', on_delete=models.CASCADE, related_name='ratings')
 
-    stars=models.IntegerField()
-    comment=models.TextField(null=True, blank=True)
+    stars=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(5)])
+    comment=models.TextField(null=True, blank=True, max_length=500,)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
     def __str__(self):
-        return f"Rating #{self.id} for Postulation #{self.id_postulation.id}"
+        return f"Rating #{self.id_rating} for Postulation #{self.id_postulation.id_postulation}"
