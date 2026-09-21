ea3606da8a9b0e136da7d3418f549b95031d17b5
Fabrizio Adrian Avila <fabri.avila3@gmail.com>
2026-09-20 21:27:28 -0300

    FEAT - Implementa MongoDB para los reportes

--- backend/apps/reports/mongodb.py
+++ backend/apps/reports/mongodb.py
@@ -8,19 +8,3 @@ db=client[settings.MONGODB_DATABASE]
 
 reports_collection = db['reports']
 
-"""
-Example document structure:
-{
-    "_id":"...",
-    "id_postulation": "...",
-    "title": "...",
-    "description": "...",
-    "steps_to_reproduce": "...",
-    "risk_level": "...",
-    "capture_evidence": "...",
-    "created_at": "...",
-    "updated_at": "...",
-    "state": "...",
-}
-
-"""
